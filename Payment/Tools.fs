bd531247e1132250c4c5c7b694acdbc7276042b1
Andrii Kurdiumov <kant2002@gmail.com>
2025-06-15 00:36:06 +0500

    Load merchants from CSV files

--- Payment/Tools.fs
+++ Payment/Tools.fs
@@ -1,6 +1,17 @@
 ﻿module Payment.Tools
 open Microsoft.AspNetCore.Http
 open Oxpecker
+open FSharp.Data
+open Core
+
+type MerchantsConfiguration = CsvProvider<"merchants.csv", Schema="Id=string,Password=string">
+
+/// Loads merchants data from CSV file
+let getMerchantsData (fileName: string) =
+    let configuration = MerchantsConfiguration.Load(fileName)
+    configuration.Rows
+        |> Seq.map (fun c -> { merchant_id = c.Id; merchant_key = c.Password })
+        |> Seq.toArray
 
 let getFlashedMessage (ctx: HttpContext) =
     match ctx.Items.TryGetValue("message") with
